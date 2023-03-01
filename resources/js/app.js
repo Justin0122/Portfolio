a2c30e86a83d2f8128d3ebe88833a0b5e0df95ed
Justin Jongstra <00300061@rijnijssel.nl>
2023-03-01 15:27:10 +0100

    active anchor style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import './scrolltotop';
 import './typewriter';
+import './anchor-element';
 import './project-carousel';
 import './aboutMe-carousel';
 import './animation-on-scroll';
